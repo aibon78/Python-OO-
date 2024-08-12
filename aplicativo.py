de63b8b1d01ff4b26614321ce563c78532555130
André Filipe <130567849+aibon78@users.noreply.github.com>
2024-08-12 17:59:13 -0300

    add Cadastro de Restaurante

--- aplicativo.py
+++ aplicativo.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Pizza',]
+
 def exibir_nome_do_programa():
     print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -23,12 +25,24 @@ def opcao_invalida():
     input('Digite uma tecla para voltar ao menu inicial: \n')
     main()
     
+def cadastrar_novo_restaurante():
+    os.system('cls')
+    print('Cadastro de novos Restaurantes\n')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    input('Digite uma tecla para voltar ao menu principal: \n')
+    main()
+    
+
+
 def escolher_opcao():
     try:
         
         opcao_escolhida = int(input('Escolha a opção: '))
         
         if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
             print('Cadastrar restaurante')
         elif opcao_escolhida == 2:
             print('Listar restaurante')
