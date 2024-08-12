91ec7292e82ce49d7056860dde0d410c1e06a7c3
André Filipe <130567849+aibon78@users.noreply.github.com>
2024-08-12 17:31:42 -0300

    add codigo invalido

--- aplicativo.py
+++ aplicativo.py
@@ -8,30 +8,41 @@ def exibir_nome_do_programa():
 ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░""")
-
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
     print('3. Ativar restaurante')
     print('4. Sair')
-
+    
 def finalizar_app():
     os.system('cls')
     print('Finalizando o App\n')
     
+def opcao_invalida():
+    print('Opção invalida\n')
+    input('Digite uma tecla para voltar ao menu inicial: \n')
+    main()
+    
 def escolher_opcao():
-    
-    opcao_escolhida = int(input('Escolha a opção: '))
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurante')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante')
-    else:
-        finalizar_app()
-    
+    try:
+        
+        opcao_escolhida = int(input('Escolha a opção: '))
+        
+        if opcao_escolhida == 1:
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar restaurante')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:    
+            finalizar_app() 
+        else: 
+            opcao_invalida()
+    except ValueError:
+        opcao_invalida()
+        
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
