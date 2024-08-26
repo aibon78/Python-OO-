61bfac8dfe15f9b1c3f1f1ac838e52dc8eba86c0
André Filipe <130567849+aibon78@users.noreply.github.com>
2024-08-26 17:57:54 -0300

    add docs

--- restaurante.py
+++ restaurante.py
@@ -4,22 +4,30 @@ class Restaurante:
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False  # Inicializa o atributo privado _ativo
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
     
-    def listar_restaurantes():
-        print(f'{'Nome do restaurante'.ljust(25)}, | {'Categoria'.ljust(25)} | {'Status'}')
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
-
     @property
     def ativo(self):
         return '☑' if self._ativo else '☐'
 
+    @ativo.setter
+    def ativo(self, valor):
+        self._ativo = valor
+
+    @staticmethod
+    def listar_restaurantes():
+        print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+
+# Criação dos objetos Restaurante
 restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_praca.ativo = True
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
+# Listar os restaurantes
 Restaurante.listar_restaurantes()
