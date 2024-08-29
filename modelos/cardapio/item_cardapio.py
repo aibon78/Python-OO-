ba26f335ce3d18011db25b70e23d21781446dc75
André Filipe <130567849+aibon78@users.noreply.github.com>
2024-08-28 22:44:31 -0300

    Correção de erros

--- modelos/cardapio/item_cardapio.py
+++ modelos/cardapio/item_cardapio.py
@@ -2,9 +2,9 @@ from abc import ABC, abstractmethod
 
 class ItemCardapio(ABC):
     def __init__(self,nome,preco):
-        self.nome = nome
-        self_preco = preco
-        
+        self._nome = nome
+        self._preco = preco
+
     @abstractmethod
     def aplicar_desconto(self):
         pass
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,5 +1,6 @@
 from modelos.avaliacao import Avaliacao
-from modelos.cardapio.item_cardapio import ItemCardapio 
+from modelos.cardapio.item_cardapio import ItemCardapio
+
 class Restaurante:
     restaurantes = []
 
@@ -9,7 +10,6 @@ class Restaurante:
         self._ativo = False
         self._avaliacao = []
         self._cardapio = []
-        
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
@@ -17,7 +17,7 @@ class Restaurante:
     
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Avaliação".ljust(25)} |{"Status"}')
+        print(f"{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} |{'Status'}")
         for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} |{restaurante.ativo}')
 
@@ -41,27 +41,21 @@ class Restaurante:
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas / quantidade_de_notas, 1)
         return media
-    
-#    def adicionar_bebida_no_cardapio(self,bebida):
- #       self._cardapio.append(bebida)
-    
-#    def adicionar_prato_no_cardapio(self,prato):
-    #    self._cardapio.append(prato)
-        
-        
+
     def adicionar_no_cardapio(self,item):
         if isinstance(item,ItemCardapio):
             self._cardapio.append(item)
     
     @property
     def exibir_cardapio(self):
-        print(f"Cardapio do restaurante {self._nome}\n")
-        for i, item in enumerate(self._cardapio,start=1):
-            if hasattr(item,'descricao' ):
-                mensagem_prato = f"{i}. Nome :{item._nome} | Preco: R${item._preco} | 
-                Descrição: {item.descricao}"
+        print(f'Cardapio do restaurante {self._nome}\n')
+        for i,item in enumerate(self._cardapio,start=1):
+            if hasattr(item,'descricao'):
+                mensagem_prato = f'{i}. Nome: {item._nome} | Preço: R${item._preco} | Descrição: {item.descricao}'
                 print(mensagem_prato)
-            else
-                mensagem_prato = f"{i}. Nome :{item._nome} | Preco: R${item._preco} |
-                Tamanho: {item.tamanho}"
+            else:
+                mensagem_bebida = f'{i}. Nome: {item._nome} | Preço: R${item._preco} | Tamanho: {item.tamanho}'
                 print(mensagem_bebida)
+
+
+    
