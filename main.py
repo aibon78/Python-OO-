a5f4c24cecb50268537fdab95d7eee4d4a13eb86
André Filipe <130567849+aibon78@users.noreply.github.com>
2024-08-30 15:28:05 -0300

    Consultando um endpoint

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-from fastapi import FastAPI
+import requests
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 @app.get('/api/hello')
 def hello_world():
     return {'Hello':'Wold'}
+
+@app.get('api/restaurante/M')
+def get_restaurante(restaurante: str = Query(None)):
+    url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+    response = requests.get(url)
+    
+
+    if response.status_code == 200:
+        dados_json = response.json()
+        if restaurante is None:
+            return {'Dados':dados_json}
+        
+        dados_restaurante=[]
+        for item in dados_json:
+            if item['Company'] == restaurante:
+            dados_restaurante.append({
+                "item": item['Item'],
+                "price": item['price'],
+                "description": item['description'],
+                
+                
+            })      
+            
+            
+        return {'Restaurante':restaurante,'Cardapio':dados_restaurante}
+    else:
+        return {'Erro':f'{response.status_code} - {response.text}'}
